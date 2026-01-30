6157dcdc30a87e1084b10279a8bcfd1a67acd5d5
dipongkorroy000 <dipongkorroy000@gmail.com>
2026-01-30 23:34:57 +0600

    learning docker connections

---
+++ src/app/config/index.ts
@@ -0,0 +1,23 @@
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({path: path.join((process.cwd(), ".env"))});
+
+export default {
+  NODE_ENV: process.env.NODE_ENV,
+  port: process.env.PORT,
+  mongo_database_url: process.env.DB_URL,
+  
+  postgres_database_url: process.env.POSTGRES_DB_URL,
+
+  mongo_container_database_url: process.env.MONGO_CONTAINER_DB_URL,
+
+  jwt_access_secret: process.env.JWT_ACCESS_SECRET,
+  jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN,
+  jwt_refresh_secret: process.env.JWT_REFRESH_SECRET,
+  jwt_refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN,
+  bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUND,
+  cloudinary_cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  cloudinary_api_key: process.env.CLOUDINARY_API_KEY,
+  cloudinary_api_secret: process.env.CLOUDINARY_API_SECRET,
+};
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,28 @@
 import {Server} from "http";
 import app from "./app";
 import {errorLogger, logger} from "./app/src/shared/logger";
-import dotenv from "dotenv";
-dotenv.config();
+import mongoose from "mongoose";
+import config from "./app/config";
+import { Pool } from "pg";
 
 let server: Server;
 
+// PostgreSQL connection pool
+ const pgPool = new Pool({ connectionString: config.postgres_database_url});
+
 async function main() {
   try {
-    server = app.listen(process.env.PORT, () => {
-      console.log(`app is listening on port ${process.env.PORT}`);
-      logger.info(`app is listening on port ${process.env.PORT}`);
+    // Connect MongoDB
+    await mongoose.connect(config.mongo_container_database_url as string);
+    logger.info("✅ Connected to MongoDB");
+
+    // Connect PostgreSQL
+    // await pgPool.connect();
+    //  logger.info("✅ Connected to PostgreSQL");
+
+    server = app.listen(config.port, () => {
+      console.log(`app is listening on port ${config.port}`);
+      logger.info(`app is listening on port ${config.port}`);
     });
   } catch (err) {
     console.log(err);
